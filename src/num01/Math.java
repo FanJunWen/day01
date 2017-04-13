b0a1027b41caef1d62bcec5e767fc0ae5744b053
FanJunWen <441697771@qq.com>
2017-04-13 14:21:30 +0800

    first commit

---
+++ src/num01/Math.java
@@ -0,0 +1,14 @@
+package num01;
+
+import java.util.Scanner;
+
+public class Math {
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num=(int)(Math.random()*1000+1);
+		System.out.println(num);
+	}
+
+
+}
