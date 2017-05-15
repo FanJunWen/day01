a492122f1b1556ac24583329236a811e3bfc6e6f
FanJunWen <441697771@qq.com>
2017-05-15 16:16:37 +0800

    two

--- src/num01/Math.java
+++ src/num01/Math.java
@@ -6,8 +6,8 @@ public class Math {
 	
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int num=(int)(Math.random()*1000+1);
-		System.out.println(num);
+//		int num=(int)(Math.random()*1000+1);
+		System.out.println(sc);
 	}
 
 
